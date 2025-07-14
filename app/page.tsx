11624ddaa8f801e08df8dc3419192e0e60c8ab3c
Gerosd <gerosdd@gmail.com>
2025-07-14 12:29:31 +0300

    made main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className={styles.page}>
-            
+        <div className={styles.mainPage}>
+            <p>Перейдите по кнопке ниже на интересующую вас таблицу</p>
+            <div className={styles.pageLinks}>
+                <Link href="/orders" className={styles.nfButton}>Заказы</Link>
+                <Link href="/sales" className={styles.nfButton}>Продажи</Link>
+            </div>
         </div>
     );
 }
